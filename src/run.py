87eed2221e367efb21ac4dc6f34de48758af0c9c
dudenzz <kubadt@gmail.com>
2025-03-25 14:32:34 +0100

    pydoc comments

--- src/run.py
+++ src/run.py
@@ -4,14 +4,16 @@ from tqdm import tqdm
 from RatingSystem import RatingSystem, RatingSystemCompetition
 from SampleSystems import NaiveRating, AverageMovieRating, GlobalAverageMovieRating, Cheater, AverageUserRating
 from system111333 import MySystem
+
 def main():
-    with open('data/movie.csv', encoding='utf-8') as file:
+    #read the movie indices
+    with open('../data/movie.csv', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         csv_reader.__next__()
         for line in csv_reader:
             Movie(int(line[0]), line[1])
-
-    with open('data/rating.csv', encoding='utf-8') as file:
+    #read the user indices
+    with open('../data/rating.csv', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
         csv_reader.__next__()
         for line in tqdm(csv_reader, total=20000263):
@@ -19,14 +21,18 @@ def main():
                 User(int(line[0]))
             User.index[int(line[0])].add_rating(Movie.index[int(line[1])],float(line[2]))
     
+    #create the competition
     competition = RatingSystemCompetition()
+    #register systems
     competition.register(MySystem())
     competition.register(NaiveRating())
     competition.register(AverageMovieRating())
     competition.register(GlobalAverageMovieRating())
     competition.register(Cheater())
     competition.register(AverageUserRating())
+    #build the matchup table
     competition.build_round_robin()
+    #run the competition - it prints out the results
     competition.compete()
        
 if __name__ == "__main__":
