342ec1e2a4d85acf660c6f954b4a408dcd789252
Łukasz Grobelny <lukasz.grobelnyyy@gmail.com>
2026-03-08 21:07:25 +0100

    Improve MySystem and add it to competition

--- src/run.py
+++ src/run.py
@@ -2,37 +2,50 @@ import csv
 from RatingLib import User, Movie
 from tqdm import tqdm
 from RatingSystem import RatingSystem, RatingSystemCompetition
-from SampleSystems import NaiveRating, AverageMovieRating, GlobalAverageMovieRating, Cheater, AverageUserRating
+from SampleSystems import (
+    NaiveRating,
+    AverageMovieRating,
+    GlobalAverageMovieRating,
+    Cheater,
+    AverageUserRating,
+)
+from system155898 import MySystem
+
 # from secret_system import MySystem as secret
 
+
 def main():
-    #read the movie indices
-    with open('../data/movie.csv', encoding='utf-8') as file:
+    # Read the movie indices
+    with open("../data/movie.csv", encoding="utf-8") as file:
         csv_reader = csv.reader(file)
         csv_reader.__next__()
         for line in csv_reader:
             Movie(int(line[0]), line[1])
-    #read the user indices
-    with open('../data/rating.csv', encoding='utf-8') as file:
+    # Read the user indices
+    with open("../data/rating.csv", encoding="utf-8") as file:
         csv_reader = csv.reader(file)
         csv_reader.__next__()
         for line in tqdm(csv_reader, total=20000263):
             if not int(line[0]) in User.index.keys():
                 User(int(line[0]))
-            User.index[int(line[0])].add_rating(Movie.index[int(line[1])],float(line[2]))
-    
-    #create the competition
+            User.index[int(line[0])].add_rating(
+                Movie.index[int(line[1])], float(line[2])
+            )
+
+    # Create the competition
     competition = RatingSystemCompetition()
-    #register systems
+    # Register systems
     # competition.register(secret())
+    competition.register(MySystem())
     competition.register(NaiveRating())
     competition.register(AverageMovieRating())
     competition.register(GlobalAverageMovieRating())
     competition.register(Cheater())
-    competition.register(AverageUserRating())    
+    competition.register(AverageUserRating())
     competition.build_round_robin()
-    #run the competition - it prints out the results
+    # Run the competition - it prints out the results
     competition.compete()
+
 
 if __name__ == "__main__":
     main()
--- src/system155898.py
+++ src/system155898.py
@@ -1,19 +1,25 @@
 from RatingSystem import RatingSystem
 
+
 class MySystem(RatingSystem):
     def __init__(self):
         super().__init__()
+        # tutaj wczytać dodatkowe dane
 
     def rate(self, user, movie):
         """
         Ta metoda zwraca rating w skali 1-5. Jest to ocena przyznana przez użytkownika 'user' filmowi 'movie'.
         """
-        if movie in user.ratings:
-            return user.ratings[movie]
+        n = len(user.ratings.values())
+        if n == 0:
+            return 3
         else:
-            return 2.5
+            return sum(user.ratings.values()) / (n - 1)
+        # tutaj usprawnić działanie 
+
     def __str__(self):
         """
         Ta metoda zwraca numery indeksów wszystkich twórców rozwiązania. Poniżej przykład.
         """
-        return 'System created by 111333 and 333444'
+        return "System created by 155898 and 000000 and 000000"
+        # tutaj uzupełnić indeksy
