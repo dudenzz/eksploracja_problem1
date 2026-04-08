13f842f620f3b7b4a9ddbb18cc22a7dbd3eef3e1
Jakub Butkiewicz <jakub.butkiewicz@student.put.poznan.pl>
2026-04-08 20:08:33 +0200

    prep for merge to upstream

--- src/run.py
+++ src/run.py
@@ -3,6 +3,7 @@ from RatingLib import User, Movie
 from tqdm import tqdm
 from RatingSystem import RatingSystem, RatingSystemCompetition
 from SampleSystems import NaiveRating, AverageMovieRating, GlobalAverageMovieRating, Cheater, AverageUserRating
+from system156007 import MySystem
 # from secret_system import MySystem as secret
 
 def main():
@@ -11,7 +12,21 @@ def main():
         csv_reader = csv.reader(file)
         csv_reader.__next__()
         for line in csv_reader:
-            Movie(int(line[0]), line[1])
+            Movie(int(line[0]), line[1],line[2].split('|'))
+    #read the genome scores
+    last_movie = None
+    with open('../data/genome_scores.csv', encoding='utf-8') as file:
+        csv_reader = csv.reader(file)
+        csv_reader.__next__()
+        for line in tqdm(csv_reader, total=11709768):
+            movie_id = int(line[0])
+            if last_movie == movie_id:
+                if movie_id in Movie.index:
+                    Movie.index[movie_id].add_genome_score(int(line[1]), float(line[2]))
+            elif last_movie:
+                if movie_id in Movie.index:
+                    Movie.index[last_movie].sort_tags()
+            last_movie = movie_id
     #read the user indices
     with open('../data/rating.csv', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
@@ -29,7 +44,8 @@ def main():
     competition.register(AverageMovieRating())
     competition.register(GlobalAverageMovieRating())
     competition.register(Cheater())
-    competition.register(AverageUserRating())    
+    competition.register(AverageUserRating())
+    competition.register(MySystem())
     competition.build_round_robin()
     #run the competition - it prints out the results
     competition.compete()
